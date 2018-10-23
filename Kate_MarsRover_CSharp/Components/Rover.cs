00d0bb38b60551eb3535c03387e73405bfdf5dd2
Cody Lee <clee@sentryone.com>
2018-10-22 22:04:15 -0400

    Added unit test project, and created first test, as well as the Rover class

---
+++ Kate_MarsRover_CSharp/Components/Rover.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata_MarsRover_CSharp_Tests.Components
+{
+    class Rover
+    {
+
+        private int X { get; set; }
+        private int Y { get; set; }
+
+        public Rover(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+    }
+}
