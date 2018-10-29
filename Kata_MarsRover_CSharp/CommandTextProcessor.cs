1e1eee3206953bb28cf0bdbddd3d6f7a6b93c93c
Cody Lee <clee@sentryone.com>
2018-10-29 14:50:00 -0400

    Build error currently, didn't get a chance to fix that. Started the TDD cycle for trying to process a null text command.

---
+++ Kata_MarsRover_CSharp/CommandTextProcessor.cs
@@ -0,0 +1,22 @@
+﻿using Kata_MarsRover_CSharp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata_MarsRover_CSharp
+{
+    public class CommandTextProcessor
+    {
+
+        public Command CreateCommandFromText(string textCommand)
+        {
+            if (textCommand == null)
+                throw new ArgumentNullException(textCommand, $"The argument {nameof(textCommand)} was null!");
+
+            return new Command();
+        }
+
+    }
+}
---
+++ Kata_MarsRover_CSharp_Tests/CommandTextProcessorTests/WhenProcessingACommand.cs
@@ -0,0 +1,25 @@
+﻿using Kata_MarsRover_CSharp;
+using Kata_MarsRover_CSharp.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata_MarsRover_CSharp_Tests.CommandTextProcessorTests
+{
+    [TestClass]
+    public class WhenProcessingACommand
+    {
+
+        [TestMethod]
+        public void WhenTheCommandIsNullAnExceptionIsThrown()
+        {
+            CommandTextProcessor commandTextProcessor = new CommandTextProcessor();
+
+            Assert.ThrowsException<ArgumentNullException>(commandTextProcessor.CreateCommandFromText(null));
+        }
+
+    }
+}
