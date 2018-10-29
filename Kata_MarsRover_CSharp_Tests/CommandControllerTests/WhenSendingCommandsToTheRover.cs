6e81428f022bc2b5bf3ee72da302dce79ebdeb28
Cody Lee <codyjleeapps@icloud.com>
2018-10-29 14:07:07 -0400

    Adding test class for the command processor

---
+++ Kata_MarsRover_CSharp_Tests/CommandControllerTests/WhenSendingCommandsToTheRover.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata_MarsRover_CSharp_Tests.CommandControllerTests
+{
+    [TestClass]
+    public class WhenSendingCommandsToTheRover
+    {
+
+        //[TestMethod]
+        //public void WhenTheCommandIsNullAnExceptionIsThrown()
+        //{
+
+        //}
+
+    }
+}
