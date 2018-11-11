48ac3b693115e52bcca0687cbfdbd0cae6ab1029
Cody Lee <codyjleeapps@icloud.com>
2018-11-11 11:19:41 -0500

    Implementing TDD cycle for creating a FileLogger with no file path.

---
+++ Kata_MarsRover_CSharp/FileLogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata_MarsRover_CSharp
+{
+    public class FileLogger
+    {
+
+        public FileLogger(string filePath)
+        {
+            if (filePath == null)
+                throw new ArgumentNullException($"The argument {nameof(filePath)} was null!");
+        }
+
+    }
+}
---
+++ Kata_MarsRover_CSharp_Tests/LoggerTests/WhenCreatingALogger.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Kata_MarsRover_CSharp;
+
+namespace Kata_MarsRover_CSharp_Tests.LoggerTests
+{
+    [TestClass]
+    public class WhenCreatingALogger
+    {
+
+        [TestMethod]
+        public void WithNoFilePathThenTheLoggerThrowsAnException()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => new FileLogger(null));
+        }
+
+    }
+}
